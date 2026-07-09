384bde584c37b4e511168b121b6adeba1183adbd
Alaa Ayyach <alaa.s.ayyach@gmail.com>
2026-07-08 19:53:25 -0700

    First and third test case

--- LeapYears/Pair1/src/test/java/edu/pdx/cs/joy/pair1/LeapYearsTest.java
+++ LeapYears/Pair1/src/test/java/edu/pdx/cs/joy/pair1/LeapYearsTest.java
@@ -1,8 +1,7 @@
 package edu.pdx.cs.joy.pair1;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.equalTo;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +17,24 @@ public class LeapYearsTest
   void isDivisibleBy400() {
     int year = 400;
     assertThat(LeapYears.isDivisibleBy400(year), equalTo(true));
+  }
+
+  @Test
+  void isNotDivisibleBy400() {
+    int year = 320;
+     assertThat(LeapYears.isDivisibleBy400(year), equalTo(false));
+  }
+
+  @Test
+  void isDivisibleBy4Not100() {
+    int year = 16; 
+    assertThat(LeapYears.isDivisibleBy4Not100(year), equalTo(true));
+  }
+
+  @Test
+  void isDivisibleBy4And100() {
+    int year = 400;
+    assertThat(LeapYears.isDivisibleBy4Not100(year), equalTo(false));
   }
 
 }
