5f496d0b558beb64db1cc678ed67b2cc0ad879b3
David Whitlock <david.m.whitlock@gmail.com>
2026-07-13 20:02:33 -0700

    Remove the string constant for the name of Sulfuras.

--- GildedRose/src/main/java/com/gildedrose/Sulfuras.java
+++ GildedRose/src/main/java/com/gildedrose/Sulfuras.java
@@ -1,9 +1,8 @@
 package com.gildedrose;
 
 public class Sulfuras extends MutableItem {
-  static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
   public Sulfuras(int sellIn, int quality) {
-    super(SULFURAS, sellIn, quality);
+    super("Sulfuras, Hand of Ragnaros", sellIn, quality);
   }
 }
