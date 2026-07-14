bca569895da3e3391b594e9f706d90c7fbaf1636
David Whitlock <david.m.whitlock@gmail.com>
2026-07-13 18:52:46 -0700

    Added some unit tests for the GildedRose.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,11 @@
 package com.gildedrose;
 
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
@@ -16,4 +19,25 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    private static @NonNull Item updateQuality(String name, int sellIn, int quality) {
+        Item item = new Item(name, sellIn, quality);
+        Item[] items = new Item[] {item};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return item;
+    }
+
+    @Test
+    void regularItemDecreasesQualityAndSellInByOne() {
+        Item item = updateQuality("Regular Item", 10, 20);
+        assertThat(item.sellIn, equalTo(9));
+        assertThat(item.quality, equalTo(19));
+    }
+
+    @Test
+    void agedBrieQualityIncreasesAtEndOfDay() {
+        Item item = updateQuality("Aged Brie", 2, 0);
+        assertThat(item.sellIn, equalTo(1));
+        assertThat(item.quality, equalTo(1));
+    }
 }
