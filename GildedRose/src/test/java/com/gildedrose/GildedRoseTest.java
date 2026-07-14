87accda704a9c1ac27568039b29915cedcf2aa14
David Whitlock <david.m.whitlock@gmail.com>
2026-07-13 19:13:24 -0700

    Added a bunch more unit tests.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -20,10 +20,16 @@ class GildedRoseTest {
     }
 
     private static @NonNull Item updateQuality(String name, int sellIn, int quality) {
+        return updateQuality(name, sellIn, quality, 1);
+    }
+
+    private static @NonNull Item updateQuality(String name, int sellIn, int quality, int numberOfDays) {
         Item item = new Item(name, sellIn, quality);
         Item[] items = new Item[] {item};
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
+        for (int i = 0; i < numberOfDays; i++) {
+            app.updateQuality();
+        }
         return item;
     }
 
@@ -47,5 +53,63 @@ class GildedRoseTest {
         Item item = updateQuality("Aged Brie", 2, 50);
         assertThat(item.sellIn, equalTo(1));
         assertThat(item.quality, equalTo(50));
+    }
+
+    @Disabled("Conjured Items not supported yet")
+    @Test
+    void qualityOfConjuredItemDecreasesBy2() {
+        Item item = updateQuality("Conjured Item", 3, 6);
+        assertThat(item.sellIn, equalTo(2));
+        assertThat(item.quality, equalTo(4));
+    }
+
+    @Test
+    void qualityOfBackstagePassesIs0AfterSellInDate() {
+        Item item = updateQuality("Backstage passes to a TAFKAL80ETC concert", 0, 57);
+        assertThat(item.sellIn, equalTo(-1));
+        assertThat(item.quality, equalTo(0));
+    }
+
+    @Test
+    void qualityOfRegularItemDecreasesBy2AfterSellInDate() {
+        Item item = updateQuality("Regular Item", 0, 20);
+        assertThat(item.sellIn, equalTo(-1));
+        assertThat(item.quality, equalTo(18));
+    }
+
+
+    @Test
+    void qualityOfBackstagePassIncreasesBy1MoreThan10DaysBeforeSellInDate() {
+        Item item = updateQuality("Backstage passes to a TAFKAL80ETC concert", 17, 20, 7);
+        assertThat(item.sellIn, equalTo(10));
+        assertThat(item.quality, equalTo(27));
+    }
+
+    @Test
+    void qualityOfBackstagePassIncreasesBy2Between10And5DaysBeforeSellInDate() {
+        Item item = updateQuality("Backstage passes to a TAFKAL80ETC concert", 10, 20, 5);
+        assertThat(item.sellIn, equalTo(5));
+        assertThat(item.quality, equalTo(30));
+    }
+
+    @Test
+    void qualityOfBackstagePassIncreasesBy3DuringThe5DaysBeforeSellInDate() {
+        Item item = updateQuality("Backstage passes to a TAFKAL80ETC concert", 5, 20, 5);
+        assertThat(item.sellIn, equalTo(0));
+        assertThat(item.quality, equalTo(35));
+    }
+
+    @Test
+    void qualityOfBackstagePassStays0AfterSellInDate() {
+        Item item = updateQuality("Backstage passes to a TAFKAL80ETC concert", -1, 0);
+        assertThat(item.sellIn, equalTo(-2));
+        assertThat(item.quality, equalTo(0));
+    }
+
+    @Test
+    void qualityOfSulfurasNeverChanges() {
+        Item item = updateQuality("Sulfuras, Hand of Ragnaros", 0, 80);
+        assertThat(item.sellIn, equalTo(0));
+        assertThat(item.quality, equalTo(80));
     }
 }
