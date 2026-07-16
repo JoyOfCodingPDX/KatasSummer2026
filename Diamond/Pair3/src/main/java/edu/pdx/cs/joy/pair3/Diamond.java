c24555f983d9e22fc77aaa1ff833f4387c27ed4f
TrevorWT <trevorwt95@gmail.com>
2026-07-15 20:16:56 -0700

    half way

--- Diamond/Pair3/src/main/java/edu/pdx/cs/joy/pair3/Diamond.java
+++ Diamond/Pair3/src/main/java/edu/pdx/cs/joy/pair3/Diamond.java
@@ -13,13 +13,31 @@ public class Diamond {
   @VisibleForTesting
   public static void main(String[] args) {
     int fromA = (int)'C' - (int)'A';
+    int outerSpace = fromA;
+    int innerSpace = 1;
 
-    for (int i = 65; i <= fromA + 65; i++) {
-      for (int j = fromA +1 ; j <= 0; j-- ) {
-        System.out.print(" ");
-      }
+
+    for (int a = 0; a <= fromA ; a++) {
+      System.out.print(' ');
+    }
+    outerSpace -= 1;
+    System.out.println('A');
+
+    for (int i = 66; i <= fromA + 65; i++) {
+
       char letter = (char)i;
+      for (int j = 0; j <= outerSpace; j++){
+        System.out.print(' ');
+      }
+      outerSpace -= 1;
+      System.out.print(letter);
+      for (int k = 0; k < innerSpace; k++){
+        System.out.print(' ');
+      }
+      innerSpace += 2;
       System.out.println(letter);
+
+
     }
   }
 }
