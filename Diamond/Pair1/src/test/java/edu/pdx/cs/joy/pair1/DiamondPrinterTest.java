cc89bee400f4ce77b6ad5b521c05f956bf58438d
Lelano <Adam.Micheal.Marion@gmail.com>
2026-07-15 19:28:00 -0700

    pair 1

---
+++ Diamond/Pair1/src/test/java/edu/pdx/cs/joy/pair1/DiamondPrinterTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.pair1;
+
+import org.junit.jupiter.api.Test;
+
+public class DiamondPrinterTest {
+
+    @Test
+    void printsTheLetterAOnASingleLine() {
+        DiamondPrinter testSetup = new DiamondPrinter("A");
+        //assertEquals(testSetup.toString() , "A");
+    }
+
+}
