22e1bcdaf61dd4a604ffb2cbad1e83f82051c492
Lelano <Adam.Micheal.Marion@gmail.com>
2026-07-27 19:41:18 -0700

    Deciding player scores.

--- Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
@@ -10,4 +10,11 @@ public class TennisTest
     new Tennis();
   }
 
+  @Test
+  void initiallyScoreIsLoveLove() {
+    Tennis tennis = new Tennis();
+    GameScore gamesState = tennis.getScore();
+    assertEquals(PlayerScore.LOVE, gamesState.getPlayerOneScore());
+    assertEquals(PlayerScore.LOVE, gamesState.getPlayerTwoScore());
+  }
 }
