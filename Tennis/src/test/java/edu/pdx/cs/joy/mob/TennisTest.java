4485e657740c143274a3460f3f60d027e4fd1133
deboyesakho <deboyesakho@gmail.com>
2026-07-27 20:19:22 -0700

    tennis passing test

--- Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
@@ -2,6 +2,8 @@ package edu.pdx.cs.joy.mob;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TennisTest
 {
 
@@ -13,8 +15,10 @@ public class TennisTest
   @Test
   void initiallyScoreIsLoveLove() {
     Tennis tennis = new Tennis();
-    GameScore gamesState = tennis.getScore();
-    assertEquals(PlayerScore.LOVE, gamesState.getPlayerOneScore());
-    assertEquals(PlayerScore.LOVE, gamesState.getPlayerTwoScore());
+    Tennis.GameScore gamesState = tennis.getScore();
+    assertEquals(Tennis.GameScore.PlayerScore.LOVE, gamesState.getPlayerOneScore());
+    assertEquals(Tennis.GameScore.PlayerScore.LOVE, gamesState.getPlayerTwoScore());
   }
+
+
 }
