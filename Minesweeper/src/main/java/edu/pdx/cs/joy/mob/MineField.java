8b76aa4cca422ae94ce36b37d37eb1884a6a27c7
TrevorWT <trevorwt95@gmail.com>
2026-08-05 19:32:18 -0700

    trevor

--- Minesweeper/src/main/java/edu/pdx/cs/joy/mob/MineField.java
+++ Minesweeper/src/main/java/edu/pdx/cs/joy/mob/MineField.java
@@ -3,10 +3,16 @@ package edu.pdx.cs.joy.mob;
 public class MineField {
     private int rows;
     private int columns;
+    private static int[][] grid;
 
     public MineField(int n, int m) {
         this.rows = n;
         this.columns = m;
+        this.grid = new int[n][m];
+    }
+
+    public int[][] getMineField() {
+        return this.grid;
     }
 
     public int size() {
--- Minesweeper/src/test/java/edu/pdx/cs/joy/mob/MineFieldTest.java
+++ Minesweeper/src/test/java/edu/pdx/cs/joy/mob/MineFieldTest.java
@@ -3,6 +3,7 @@ package edu.pdx.cs.joy.mob;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MineFieldTest {
@@ -11,6 +12,12 @@ public class MineFieldTest {
     void sweepTest() {
         MineField field = new MineField(4, 4);
         assertEquals(field.size(), 16);
+    }
+
+    @Test
+    void initializeTest() {
+        MineField field = new MineField(2, 2);
+        assertThat(new int[2][2], equalTo(field.getMineField()));
     }
 
 }
